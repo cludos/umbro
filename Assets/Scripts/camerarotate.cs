66ec08c7cede82af659926d64d046a0970340f72
cludos <cludosmit@gmail.com>
2017-02-27 16:20:28 -0500

    add camerarotate script

---
+++ Assets/Scripts/camerarotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camerarotate : MonoBehaviour {
+    public GameObject board;
+    private Transform target;
+
+	// Use this for initialization
+	void Start () {
+        target = board.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0)) {
+            transform.LookAt(target);
+            transform.RotateAround(target.position, Vector3.up, Input.GetAxis("Mouse X") * 1);
+        }
+	}
+}
